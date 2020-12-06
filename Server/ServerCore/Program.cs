33ee0c16281ae9a5ec9f7548f91a7e463d48e13c
JangHyoenJun <totok682@naver.com>
2020-12-06 21:49:09 +0900

    멀티스레드 공부 Thread, ThreadPool, Task

--- Server/ServerCore/Program.cs
+++ Server/ServerCore/Program.cs
@@ -1,12 +1,49 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace ServerCore
 {
     class Program
     {
+        static void MainThread(object state)
+        {
+            for (int i=0; i<5; i++)
+            {
+                Console.WriteLine("Hello Thread!");
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ThreadPool.SetMinThreads(1, 1);
+            ThreadPool.SetMaxThreads(5, 5);
+            for (int i = 0; i < 5; i++)
+                ThreadPool.QueueUserWorkItem((obj) => { while (true) {  } });
+
+            for (int i = 0; i < 5; i++)
+            {
+                Task t = new Task(() => { while (true) { Console.WriteLine("working..."); } }, TaskCreationOptions.LongRunning);
+                t.Start();
+            }
+
+
+            
+
+            ThreadPool.QueueUserWorkItem(MainThread);
+
+            //Thread t = new Thread(MainThread);
+            //t.Name = "Test Thread";
+            //t.IsBackground = true;
+            //t.Start();
+            //Console.WriteLine("Waiting for Thread!");
+            //t.Join();
+
+            //Console.WriteLine("Hello World!");
+
+            while (true)
+            {
+
+            }
         }
     }
 }
