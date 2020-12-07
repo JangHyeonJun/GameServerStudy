9df91a9978c7c8b4996d57d9471957ce9d94cbef
JangHyoenJun <totok682@naver.com>
2020-12-07 21:52:29 +0900

    멀티쓰레드 공부 하드웨어는 최적화를 위해 코드의 실행 순서를 바꿀수도 있다. 이는 멀티쓰레드 환경에서 문제가 된다. 이를 방지하기위해 메모리 배리어라는 것을 사용한다.

--- Server/ServerCore/Program.cs
+++ Server/ServerCore/Program.cs
@@ -4,31 +4,85 @@ using System.Threading.Tasks;
 
 namespace ServerCore
 {
+    // 메모리 배리어
+    // A) 코드 재배치 억제
+    // B) 가시성
+
+    // 1) Full Memory Barrier (ASM MFENCE, C# Thread.MemoryBarrier) : store/load 둘다 막는 것
+    // 2) Store Memory Barrie (ASM SFENCE) : store만 막는 것
+    // 3) Load Memory Barrie  (ASM LFENCE) : store만 막는 것
+
     class Program
     {
+        static int x = 0;
+        static int y = 0;
+        static int r1 = 0;
+        static int r2 = 0;
+
+        static void Thread_1()
+        {
+            y = 1; // stroe y
+
+            // -------------------- 넘어오지맛
+            Thread.MemoryBarrier(); // store, load 전/후에 메인 메모리에 있던 내용을 flush 해준다.
+
+            r1 = x; // load x
+        }
+
+        static void Thread_2()
+        {
+            x = 1; // store x
+
+            // -------------------- 넘어오지맛
+            Thread.MemoryBarrier();
+
+            r2 = y; // load y;
+        }
+
+
+        int _answer;
+        bool _complete;
+
+        void A()
+        {
+            _answer = 123; // load
+            Thread.MemoryBarrier();
+            _complete = true; // load
+            Thread.MemoryBarrier(); // load를 2번 하기 때문에 뒤에도 배리어를 두었다.
+        }
+
+        void B()
+        {
+            Thread.MemoryBarrier();
+            if (_complete) // read
+            {
+                Thread.MemoryBarrier();
+                Console.WriteLine(_answer); // read
+            }
+        }
+
         static void Main(string[] args)
         {
-            int[,] arr = new int[10000, 10000];
-
+            int count = 0;
+            while (true)
             {
-                long now = DateTime.Now.Ticks;
-                for (int y = 0; y < 10000; y++)
-                    for (int x = 0; x < 10000; x++)
-                        arr[y, x] = 1;
-                long end = DateTime.Now.Ticks;
+                count++;
+                x = y = r1 = r2 = 0;
 
-                Console.WriteLine($"(y, x) 순서 걸린 시간 {end - now}");
+                Task t1 = new Task(Thread_1);
+                Task t2 = new Task(Thread_2);
+                t1.Start();
+                t2.Start();
+
+                Task.WaitAll(t1, t2);
+
+                if (r1 == 0 && r2 == 0)
+                {
+                    break;
+                }
             }
 
-            {
-                long now = DateTime.Now.Ticks;
-                for (int y = 0; y < 10000; y++)
-                    for (int x = 0; x < 10000; x++)
-                        arr[x, y] = 1;
-                long end = DateTime.Now.Ticks;
-
-                Console.WriteLine($"(y, x) 순서 걸린 시간 {end - now}");
-            }
+            Console.WriteLine($"{count}번 만에 빠져나옴!");
         }
     }
 }
