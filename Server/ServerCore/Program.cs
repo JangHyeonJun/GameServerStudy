860881f09a2cbed9858bcd7ac780bcc7972ebfa6
JangHyoenJun <totok682@naver.com>
2020-12-07 21:29:32 +0900

    멀티 쓰레드 공부 캐시의 지역성 중, 공간적 지역성(Spatial Locality) 실습

--- Server/ServerCore/Program.cs
+++ Server/ServerCore/Program.cs
@@ -6,35 +6,29 @@ namespace ServerCore
 {
     class Program
     {
-        volatile static bool _stop = false;
-
-        static void ThreadMain()
-        {
-            Console.WriteLine("Thread Start!");
-
-            while (_stop == false)
-            {
-                // wait someone to call stop signal
-            }
-
-            Console.WriteLine("Thread Stop!");
-        }
-
         static void Main(string[] args)
         {
-            Task t = new Task(ThreadMain);
-            t.Start();
+            int[,] arr = new int[10000, 10000];
 
-            Thread.Sleep(1000);
+            {
+                long now = DateTime.Now.Ticks;
+                for (int y = 0; y < 10000; y++)
+                    for (int x = 0; x < 10000; x++)
+                        arr[y, x] = 1;
+                long end = DateTime.Now.Ticks;
 
-            _stop = true;
+                Console.WriteLine($"(y, x) 순서 걸린 시간 {end - now}");
+            }
 
-            Console.WriteLine("Call Stop");
-            Console.WriteLine("Waiting Stop");
+            {
+                long now = DateTime.Now.Ticks;
+                for (int y = 0; y < 10000; y++)
+                    for (int x = 0; x < 10000; x++)
+                        arr[x, y] = 1;
+                long end = DateTime.Now.Ticks;
 
-            t.Wait();
-
-            Console.WriteLine("Success Stop");
+                Console.WriteLine($"(y, x) 순서 걸린 시간 {end - now}");
+            }
         }
     }
 }
