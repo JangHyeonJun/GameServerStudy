2d5d5bfd2209dbdfc9f3124c86aec994706717b2
JangHyoenJun <totok682@naver.com>
2020-12-12 23:56:17 +0900

    멀티쓰레드 공부 데드락 실습

--- Server/ServerCore/Program.cs
+++ Server/ServerCore/Program.cs
@@ -4,32 +4,75 @@ using System.Threading.Tasks;
 
 namespace ServerCore
 {
+    class FastLock // 락에 번호를 매겨서 번호 순으로 락을 걸지 않으면 크래시, 그래프에 사이클이 있는지 확인하는 용도로 사용 가능.
+    {
+        public int id;
+    }
+
+    class SessionManager
+    {
+        FastLock l;
+        static object _lock = new object();
+        public static void Test()
+        {
+            lock (_lock)
+            {
+                UserManager.TestUser();
+            }
+        }
+
+        public static void TestSession()
+        {
+            lock (_lock)
+            {
+
+            }
+        }
+    }
+
+    class UserManager
+    {
+        FastLock l;
+        static object _lock = new object();
+
+        public static void Test()
+        {
+            //Monitor.TryEnter() 제한 시간 내에 락을 획득하지 못하면 실패처리 (애초에 실패가 있으면 안좋은 경우)
+
+            lock (_lock)
+            {
+                SessionManager.TestSession();
+            }
+        }
+
+        public static void TestUser()
+        {
+            lock (_lock)
+            {
+                
+            }
+        }
+    }
+
     class Program
     {
         static int number = 0;
         static object _obj = new object();
+        static object _obj2 = new object();
 
         static void Thread_1()
         {
-            for (int i = 0; i < 100000; i++)
+            for (int i = 0; i < 10000; i++)
             {
-                // 상호 배제 Mutual Exclusive
-                Monitor.Enter(_obj); // lock
-
-                number++;
-                // return; 여기서 빠져나오면 락이 해제가 안되어버림. 데드락 DeadLock
-                Monitor.Exit(_obj);
+                SessionManager.Test();
             }
         }
 
         static void Thread_2()
         {
-            for (int i = 0; i < 100000; i++)
+            for (int i = 0; i < 10000; i++)
             {
-                lock (_obj)
-                {
-                    number--;
-                }
+                UserManager.Test();
             }
         }
 
@@ -38,6 +81,9 @@ namespace ServerCore
             Task t1 = new Task(Thread_1);
             Task t2 = new Task(Thread_2);
             t1.Start();
+
+            Thread.Sleep(100); // 데드락이지만 발견하지 못할 수 있다.
+
             t2.Start();
 
             Task.WaitAll(t1, t2);
