69df81829ce4891d6568d4a930849a4c150bd149
JangHyoenJun <totok682@naver.com>
2020-12-13 16:42:58 +0900

    멀티쓰레드 공부 스핀락 실습

--- Server/ServerCore/Program.cs
+++ Server/ServerCore/Program.cs
@@ -4,91 +4,70 @@ using System.Threading.Tasks;
 
 namespace ServerCore
 {
-    class FastLock // 락에 번호를 매겨서 번호 순으로 락을 걸지 않으면 크래시, 그래프에 사이클이 있는지 확인하는 용도로 사용 가능.
+    class SpinLock
     {
-        public int id;
-    }
+        volatile int _locked = 0;
 
-    class SessionManager
-    {
-        FastLock l;
-        static object _lock = new object();
-        public static void Test()
+        public void Acquire()
         {
-            lock (_lock)
+            while(true)
             {
-                UserManager.TestUser();
+                //var original = Interlocked.Exchange(ref _locked, 1);
+                //if (original == 0)
+                //    break;
+
+                // CAS Compare-And-Swap
+                int expected = 0;
+                int desired = 1;
+                                
+                if (Interlocked.CompareExchange(ref _locked, desired, expected) == expected)
+                    break;
             }
         }
 
-        public static void TestSession()
+        public void Release()
         {
-            lock (_lock)
-            {
-
-            }
-        }
-    }
-
-    class UserManager
-    {
-        FastLock l;
-        static object _lock = new object();
-
-        public static void Test()
-        {
-            //Monitor.TryEnter() 제한 시간 내에 락을 획득하지 못하면 실패처리 (애초에 실패가 있으면 안좋은 경우)
-
-            lock (_lock)
-            {
-                SessionManager.TestSession();
-            }
-        }
-
-        public static void TestUser()
-        {
-            lock (_lock)
-            {
-                
-            }
+            _locked = 0;
         }
     }
 
     class Program
     {
-        static int number = 0;
-        static object _obj = new object();
-        static object _obj2 = new object();
+        static int _num = 0;
+        static SpinLock _lock = new SpinLock();
 
         static void Thread_1()
-        {
-            for (int i = 0; i < 10000; i++)
+        { 
+            for(int i=0; i<100000; i++)
             {
-                SessionManager.Test();
+                _lock.Acquire();
+                _num++;
+                _lock.Release();
             }
         }
 
         static void Thread_2()
         {
-            for (int i = 0; i < 10000; i++)
+            for (int i = 0; i < 100000; i++)
             {
-                UserManager.Test();
+                _lock.Acquire();
+                _num--;
+                _lock.Release();
             }
+
         }
 
         static void Main(string[] args)
         {
             Task t1 = new Task(Thread_1);
             Task t2 = new Task(Thread_2);
+
             t1.Start();
-
-            Thread.Sleep(100); // 데드락이지만 발견하지 못할 수 있다.
-
             t2.Start();
 
             Task.WaitAll(t1, t2);
 
-            Console.WriteLine(number);
+            Console.WriteLine(_num);
         }
     }
 }
