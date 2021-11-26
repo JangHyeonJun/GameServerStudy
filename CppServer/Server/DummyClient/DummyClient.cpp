e3dd74a544608b5a38c0c5825855b645865ea5c3
JangMotti <totok682@naver.com>
2021-11-26 21:56:58 +0900

    C++ 기본 소켓 서버 구현

--- CppServer/Server/DummyClient/DummyClient.cpp
+++ CppServer/Server/DummyClient/DummyClient.cpp
@@ -8,4 +8,59 @@
 
 int main()
 {
+	// 윈속 초기화 (ws2_32 라이브러리 초기화)
+	// 관련 정보가 wsaData에 채워짐
+	WSAData wsaData;
+	if (::WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+		return 0;
+
+	// af : Address Family (AF_INET = IPv4)
+	// type : TCP(SOCK_STREAM)
+	// protocol : 0
+	// return : descriptor
+	SOCKET clientSocket = ::socket(AF_INET, SOCK_STREAM, 0);
+	if (clientSocket == INVALID_SOCKET)
+	{
+		int32 errCode = ::WSAGetLastError();
+		cout << "Socket ErrorCode: " << errCode << endl;
+		return 0;
+	}
+
+
+	// 연결할 목적지는 (IP + PORT)
+	SOCKADDR_IN serverAddr; // IPv4
+	::memset(&serverAddr, 0, sizeof(serverAddr));
+	serverAddr.sin_family = AF_INET;
+	// serverAddr.sin_addr.s_addr = ::inet_addr("127.0.0.1") << DEPRECATED;
+	::inet_pton(AF_INET, "127.0.0.1", &serverAddr.sin_addr);
+
+	// host to network short
+	// Little		&  Big Endian
+	// 78|56|34|12     12|34|56|78
+	// 윈도우는 LittleEndian VS로 memory 켜보면 알 수 있다.
+	serverAddr.sin_port = ::htons(7777); 
+
+	if (::connect(clientSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR)
+	{
+		int32 errCode = ::WSAGetLastError();
+		cout << "Connect ErrorCode: " << errCode << endl;
+		return 0;
+	}
+
+	// 연결 성공! 데이터 송수신 가능!
+
+	cout << "Connected To Server!" << endl;
+
+	while (true)
+	{
+		// TODO
+		this_thread::sleep_for(1s);
+	}
+
+	// ----------------------------------
+
+	::closesocket(clientSocket);
+
+	// 윈속 종료
+	::WSACleanup();
 }
--- CppServer/Server/GameServer/GameServer.cpp
+++ CppServer/Server/GameServer/GameServer.cpp
@@ -14,4 +14,70 @@
 
 int main()
 {
+	// 윈속 초기화 (ws2_32 라이브러리 초기화)
+	// 관련 정보가 wsaData에 채워짐
+	WSAData wsaData;
+	if (::WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+		return 0;
+
+	// af : Address Family (AF_INET = IPv4)
+	// type : TCP(SOCK_STREAM)
+	// protocol : 0
+	// return : descriptor
+	SOCKET listenSocket = ::socket(AF_INET, SOCK_STREAM, 0);
+	if (listenSocket == INVALID_SOCKET)
+	{
+		int32 errCode = ::WSAGetLastError();
+		cout << "Socket ErrorCode: " << errCode << endl;
+		return 0;
+	}
+
+	// 나의 주소는 (IP + PORT)
+	SOCKADDR_IN serverAddr; // IPv4
+	::memset(&serverAddr, 0, sizeof(serverAddr));
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_addr.s_addr = ::htonl(INADDR_ANY); // 알아서 연결가능한 ip 골라줘
+	serverAddr.sin_port = ::htons(7777);
+
+	if (::bind(listenSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR)
+	{
+		int32 errCode = ::WSAGetLastError();
+		cout << "Bind ErrorCode: " << errCode << endl;
+		return 0;
+	}
+
+	// listen 할 수 있는 소켓 대기열 길이
+	if (::listen(listenSocket, 10) == SOCKET_ERROR)
+	{
+		int32 errCode = ::WSAGetLastError();
+		cout << "Listen ErrorCode: " << errCode << endl;
+		return 0;
+	}
+
+	// Listen 하여 Client 소켓을 생성
+
+	while (true)
+	{
+		SOCKADDR_IN clientAddr; // IPv4
+		::memset(&clientAddr, 0, sizeof(clientAddr));
+		int32 addrLen = sizeof(clientAddr);
+		SOCKET clientSocket = ::accept(listenSocket, (SOCKADDR*)&clientAddr, &addrLen);
+
+		if (clientSocket == INVALID_SOCKET)
+		{
+			int32 errCode = ::WSAGetLastError();
+			cout << "Accept ErrorCode: " << errCode << endl;
+			return 0;
+		}
+
+		// 연결 성공! 데이터 송수신 가능!
+		char ipAddress[16];
+		::inet_ntop(AF_INET, &clientAddr.sin_addr, ipAddress, sizeof(ipAddress));
+		cout << "Client Connected! IP = " << ipAddress << endl;
+	}
+
+	// ------------------------------
+
+	// 윈속 종료
+	::WSACleanup();
 }
