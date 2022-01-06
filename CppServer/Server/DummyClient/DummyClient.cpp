927bd19c738f3d59e99eeb02db0df56cad4e751f
JangMotti <totok682@naver.com>
2022-01-06 23:16:15 +0900

    C++ 소켓 옵션 설정

--- CppServer/Server/DummyClient/DummyClient.cpp
+++ CppServer/Server/DummyClient/DummyClient.cpp
@@ -25,57 +25,7 @@ int main()
 		return 0;
 	}
 
-
-	SOCKADDR_IN serverAddr;
-	::memset(&serverAddr, 0, sizeof(serverAddr));
-	serverAddr.sin_family = AF_INET;
-	::inet_pton(AF_INET, "127.0.0.1", &serverAddr.sin_addr);
-	serverAddr.sin_port = ::htons(7777); 
-
-	// Connected UDP
-	//::connect(clientSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr));
-
-	while (true)
-	{
-		// TODO
-		char sendBuffer[100] = "Hello World!";
-		
-		// Connected UDP
-		//int32 resultCode = ::send(clientSocket, sendBuffer, sizeof(sendBuffer), 0);
-		int32 resultCode = ::sendto(clientSocket, sendBuffer, sizeof(sendBuffer), 0,
-			(SOCKADDR*)&serverAddr, sizeof(serverAddr));
-		if (resultCode == SOCKET_ERROR)
-		{
-			HandleError("SendTo");
-			return 0;
-		}
-
-		cout << "Send Data! Len = " << sizeof(sendBuffer) << endl;
-
-
-		SOCKADDR_IN recvAddr;
-		::memset(&recvAddr, 0, sizeof(recvAddr));
-		int32 addrLen = sizeof(recvAddr);
-
-		char recvbuffer[1000];
-
-		// Connected UDP
-		//int32 recvLen = ::recv(clientSocket, recvbuffer, sizeof(recvbuffer), 0);
-		int32 recvLen = ::recvfrom(clientSocket, recvbuffer, sizeof(recvbuffer), 0,
-			(SOCKADDR*)&recvAddr, &addrLen);
-
-		if (recvLen <= 0)
-		{
-			HandleError("RecvFrom");
-			return 0;
-		}
-
-		cout << "recv data! data = " << recvbuffer << endl;
-		cout << "recv data! len = " << recvLen << endl;
-
-		this_thread::sleep_for(1s);
-	}
-
+	// 소켓 리소스 반환
 	::closesocket(clientSocket);
 
 	// 윈속 종료
--- CppServer/Server/GameServer/GameServer.cpp
+++ CppServer/Server/GameServer/GameServer.cpp
@@ -31,48 +31,48 @@ int main()
 		return 0;
 	}
 
-	SOCKADDR_IN serverAddr; // IPv4
-	::memset(&serverAddr, 0, sizeof(serverAddr));
-	serverAddr.sin_family = AF_INET;
-	serverAddr.sin_addr.s_addr = htonl(INADDR_ANY);
-	serverAddr.sin_port = ::htons(7777);
+	// 소켓 옵션 세팅 (setsockopt)
+	// 옵션을 해석하고 처리하는 단계 (level)
+	// 소켓 코드 -> SQL_SOCKET
+	// Ipv4 -> IPPROTO_IP
+	// TCP -> IPPROTO_TCP
 
-	if (::bind(serverSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR)
-	{
-		HandleError("Bind");
-		return 0;
-	}
+	// SO_KEEPALIVE = 주기적으로 연결 상태 확인 (Tcp Only) == heartbeat ?
+	bool enable = true;
+	::setsockopt(serverSocket, SOL_SOCKET, SO_KEEPALIVE, (char*)&enable, sizeof(enable));
+	
+	// SO_LINGER = 지연하다]// 송신 버퍼에 있는 데이터를 보낼것인가? 날릴것인가?
 
-	while (true)
-	{
-		SOCKADDR_IN clientAddr;
-		::memset(&clientAddr, 0, sizeof(clientAddr));
-		int32 addrLen = sizeof(clientAddr);
+	// onoff = 0이면 closesocket()이 바로 리턴, 아니면 linger 초만큼 대기 (default 0)
+	// 보내는 중인 패킷을 충분히 linger초만큼 보내고 종료한다는 뜻.
+	LINGER linger;
+	linger.l_onoff = 1;
+	linger.l_linger = 5;
+	::setsockopt(serverSocket, SOL_SOCKET, SO_LINGER, (char*)&linger, sizeof(linger));
 
-		char recvBuffer[100];
+	// Half-Close 일부만 소켓을 닫는것.
+	// SD_SEND : send를 막음
+	// SD_RECEIVE : recv를 막음
+	// SD_BOTH : 둘다 막음
+	// ::shutdown(serverSocket, SD_SEND);
+	// 소켓 리소스 반환
+	// send -> closesocket
+	//::closesocket(serverSocket);
+
+	// SO_SNDBUF = 송신 버퍼 크기
+	// SO_RCVBUF = 수신 버퍼 크기
+
+	int32 sendBufferSize;
+	int32 optionLen = sizeof(sendBufferSize);
+	::getsockopt(serverSocket, SOL_SOCKET, SO_SNDBUF, (char*)&sendBufferSize, &optionLen);
+	cout << "송신 버퍼 크기: " << sendBufferSize << endl;
+
+	int32 recvBufferSize;
+	optionLen = sizeof(recvBufferSize);
+	::getsockopt(serverSocket, SOL_SOCKET, SO_SNDBUF, (char*)&recvBufferSize, &optionLen);
+	cout << "수신 버퍼 크기: " << recvBufferSize << endl;
 
 
-		int32 recvLen = ::recvfrom(serverSocket, recvBuffer, sizeof(recvBuffer), 0,
-			(SOCKADDR*)&clientAddr, &addrLen);
-
-		if (recvLen <= 0)
-		{
-			HandleError("RecvFrom");
-			return 0;
-		}
-
-		cout << "Recv Data! Data = " << recvBuffer << endl;
-		cout << "Recv Data! Len = " << recvLen << endl;
-
-		int32 errCode = ::sendto(serverSocket, recvBuffer, recvLen, 0,
-			(SOCKADDR*)&clientAddr, sizeof(clientAddr));
-
-		if (errCode == SOCKET_ERROR)
-		{
-			HandleError("SendTo");
-			return 0;
-		}
-	}
 
 	// 윈속 종료
 	::WSACleanup();
